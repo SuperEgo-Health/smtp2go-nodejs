6d41e28199b5d67c7912c5ef913ebd7eb8795975
Immortalin <7126128+Immortalin@users.noreply.github.com>
2025-03-27 19:30:31 +0100

    Update build

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,6 @@ module.exports = {
   plugins: [
     ["@babel/plugin-transform-class-properties"],
     ["@babel/plugin-transform-typescript"],
-    ["@babel/plugin-transform-runtime"],
+    // ["@babel/plugin-transform-runtime"],
   ],
 };
